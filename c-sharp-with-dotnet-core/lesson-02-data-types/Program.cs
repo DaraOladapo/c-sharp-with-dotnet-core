0eb91ecf30d530f99acc8c4bbd0024906fa4dc55
Dara Oladapo <daraoladapo@outlook.com>
2019-06-29 14:16:13 +0100

    putting in some data type lesson stuff

--- c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
+++ c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
@@ -6,7 +6,18 @@ namespace lesson_02_data_types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //basic data types
+            //int
+            int someInt=1;
+            //double
+            double someDouble=2.5;
+            double someOtherDouble=2;
+            //float
+            float someFloat=2.0F;
+            //string
+            string someString="I am a string";
+            //char
+            char someChar='c';
         }
     }
 }
