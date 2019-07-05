e9a7fda42ece3bdb3ced97a546264482bc68379a
Dara Oladapo <daraoladapo@outlook.com>
2019-07-05 12:29:12 +0100

    additoinia

--- c-sharp-with-dotnet-core/lesson-03-variables-and-calculations/Program.cs
+++ c-sharp-with-dotnet-core/lesson-03-variables-and-calculations/Program.cs
@@ -15,6 +15,21 @@ namespace lesson_03_variables_and_calculations
             //implicit declaration
             var implicitVariable = 25;
             Console.WriteLine($"{implicitVariable} is of datatype {implicitVariable.GetType()}");
+
+            //calculations
+            int firstVal = 23, secondVal = 19;
+            int valAddition = firstVal + secondVal;
+            Console.WriteLine($"{firstVal} + {secondVal} = {valAddition}");
+            int valSubtraction = firstVal - secondVal;
+            Console.WriteLine($"{firstVal} - {secondVal} = {valSubtraction}");
+            int valMultiplication = firstVal * secondVal;
+            Console.WriteLine($"{firstVal} * {secondVal} = {valMultiplication}");
+            //intentional for next lesson
+            int valDivision = firstVal / secondVal;
+            Console.WriteLine($"{firstVal} / {secondVal} = {valDivision}");
+
+            int valRemainderAfterDiv = firstVal % secondVal;
+            Console.WriteLine($"{firstVal} remainder div of {secondVal} = {valRemainderAfterDiv}");
         }
     }
 }
