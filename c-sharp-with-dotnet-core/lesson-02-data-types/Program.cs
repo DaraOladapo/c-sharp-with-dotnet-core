b90a88dc30a82d566b5532a63cc508ce262e463b
Dara Oladapo <daraoladapo@outlook.com>
2019-07-05 08:50:34 +0100

    data types description

--- c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
+++ c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
@@ -9,15 +9,21 @@ namespace lesson_02_data_types
             //basic data types
             //int
             int someInt=1;
+            Console.WriteLine($"{someInt} is an {someInt.GetType()}");
             //double
             double someDouble=2.5;
+            Console.WriteLine($"{someDouble} is an {someDouble.GetType()}");
             double someOtherDouble=2;
+            Console.WriteLine($"{someOtherDouble} is an {someOtherDouble.GetType()}");
             //float
             float someFloat=2.0F;
+            Console.WriteLine($"{someFloat} is an {someFloat.GetType()}");
             //string
             string someString="I am a string";
+            Console.WriteLine($"{someString} is an {someString.GetType()}");
             //char
             char someChar='c';
+            Console.WriteLine($"{someChar} is an {someChar.GetType()}");
         }
     }
 }
