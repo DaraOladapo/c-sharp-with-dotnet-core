ccfb1ecf230324ff015f5dd2fbbca2fdd61fe32a
Dara Oladapo <daraoladapo@outlook.com>
2019-07-05 08:15:21 +0100

    on overloaded

--- c-sharp-with-dotnet-core/lesson-17-overloading-operators/Program.cs
+++ c-sharp-with-dotnet-core/lesson-17-overloading-operators/Program.cs
@@ -6,19 +6,21 @@ namespace lesson_17_overloading_operators
     {
         static void Main(string[] args)
         {
-            int A=5,B=6;
-            var intAdd=Add(A,B);
+            int A = 5, B = 6;
+            var intAdd = Add(A, B);
             System.Console.WriteLine(intAdd);
-            double C=4.6,D=7.6;
-            var doubeAdd=Add(C,D);
+            double C = 4.6, D = 7.6;
+            var doubeAdd = Add(C, D);
             Console.WriteLine(doubeAdd);
         }
-        
-        private static double Add(double A,double B){
-            return A+B;
+
+        private static double Add(double A, double B)
+        {
+            return A + B;
         }
-        private static int Add(int A,int B){
-            return A+B;
+        private static int Add(int A, int B)
+        {
+            return A + B;
         }
     }
 }
