b35c589f2038a59d1544c8f5aa112e1e5cd391d3
Dara Oladapo <daraoladapo@outlook.com>
2019-07-05 12:41:55 +0100

    on debugs and parsers

--- c-sharp-with-dotnet-core/lesson-04-debugging/Program.cs
+++ c-sharp-with-dotnet-core/lesson-04-debugging/Program.cs
@@ -6,7 +6,20 @@ namespace lesson_04_debugging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int firstVal = 23, secondVal = 19;
+            int valDivision = firstVal / secondVal;
+            Console.WriteLine($"{firstVal} / {secondVal} = {valDivision}");
+            //problem?
+
+            //one way to solve this
+            var valDebugDiv = Convert.ToDouble(firstVal) /Convert.ToDouble(secondVal);
+            Console.WriteLine($"{firstVal} / {secondVal} = {valDebugDiv}");
+
+            //touch on parsing values and .ToSTring()
+            Console.WriteLine($"{Convert.ToInt32("32").GetType()}");
+            Console.WriteLine($"{Convert.ToDouble("32").GetType()}");
+            Console.WriteLine($"{Convert.ToString(32.44).GetType()}");
+            Console.WriteLine($"{valDebugDiv.ToString()} is of type {valDebugDiv.ToString().GetType()} while {valDebugDiv} is of type {valDebugDiv.GetType()}");
         }
     }
 }
