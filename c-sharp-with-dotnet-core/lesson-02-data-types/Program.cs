03aa67b2b74c51fdb04d3dad00a969126a1c316c
daraoladapo <daraoladapo@outlook.com>
2019-07-07 19:22:07 +0100

    edits on data types

--- c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
+++ c-sharp-with-dotnet-core/lesson-02-data-types/Program.cs
@@ -8,27 +8,27 @@ namespace lesson_02_data_types
         {
             //basic data types
             //int
-            int someInt=1;
+            int someInt = 1;
             Console.WriteLine($"{someInt} is of datatype {someInt.GetType()}");
+            Console.WriteLine($"Data type INT has a min value of {int.MinValue} and max value of {int.MaxValue}");
             //long
             long someLong = 34232434342;
             Console.WriteLine($"{someLong} is of datatype {someLong.GetType()}");
+            Console.WriteLine($"Data type LONG has a min value of {long.MinValue} and max value of {long.MaxValue}");
             //double
-            double someDouble=2.5;
+            double someDouble = 2.5;
             Console.WriteLine($"{someDouble} is of datatype {someDouble.GetType()}");
-            double someOtherDouble=2;
+            double someOtherDouble = 2;
             Console.WriteLine($"{someOtherDouble} is of datatype {someOtherDouble.GetType()}");
-            double min = double.MinValue;
-            double max = double.MaxValue;
-            Console.WriteLine($"The range of the double type is {min} to {max}");
+            Console.WriteLine($"Data type DOUBLE has a min value of {double.MinValue} and max value of {double.MaxValue}");
             //float
-            float someFloat=2.0F;
+            float someFloat = 2.0F;
             Console.WriteLine($"{someFloat} is of datatype {someFloat.GetType()}");
             //string
-            string someString="I am a string";
+            string someString = "I am a string";
             Console.WriteLine($"{someString} is of datatype {someString.GetType()}");
             //char
-            char someChar='c';
+            char someChar = 'c';
             Console.WriteLine($"{someChar} is of datatype {someChar.GetType()}");
         }
     }
