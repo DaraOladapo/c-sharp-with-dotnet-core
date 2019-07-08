3a645694dd4cd8f6b9a5ecb01390eba0f6a6ba8f
daraoladapo <daraoladapo@outlook.com>
2019-07-08 09:03:03 +0100

    new set

--- c-sharp-with-dotnet-core/lesson-06-date-time/Program.cs
+++ c-sharp-with-dotnet-core/lesson-06-date-time/Program.cs
@@ -6,7 +6,11 @@ namespace lesson_06_date_time
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var timeRightNow = DateTime.Now;
+            Console.WriteLine($"The current date time is {timeRightNow}");
+            //now formatting
+            Console.WriteLine($"The current hour of day is {DateTime.Now.Hour}");
+            Console.WriteLine($"The current minute of the hour of day is {DateTime.Now.Minute}");
         }
     }
 }
