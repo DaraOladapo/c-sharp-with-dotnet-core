33db01ea25232a09f219e03283e964d7c0c56e9a
Dara Oladapo <daraoladapo@outlook.com>
2019-07-10 16:29:22 +0100

    enums and structures

--- c-sharp-with-dotnet-core/lesson-09-making-choices/Program.cs
+++ c-sharp-with-dotnet-core/lesson-09-making-choices/Program.cs
@@ -6,7 +6,47 @@ namespace lesson_09_making_choices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the weight of the child born?");
+            double newBornWeight=double.Parse(Console.ReadLine());
+            Console.WriteLine("What is the gender? 1 for male, 2 for female");
+            int newBornGender=int.Parse(Console.ReadLine());
+            WeightClass weightClass;
+            if (newBornWeight<1.0)
+            {
+                weightClass=WeightClass.UnderWeight;
+            }
+            else if(newBornWeight >= 1.0 && newBornWeight <= 2.0 )
+            {
+             weightClass=WeightClass.Normal;
+            }
+            else
+            {
+                weightClass=WeightClass.Overweight;
+            }
+            Child newBorn=new Child
+            {
+                Weight=newBornWeight,
+                Gender=(Gender)newBornGender,
+                WeightClass=weightClass
+            };
+ Console.WriteLine($"A {newBorn.Gender} child was born and weighs {newBorn.Weight}kg and is {newBorn.WeightClass}");
+       
         }
+    }
+    enum WeightClass{
+        UnderWeight,
+        Normal,
+        Overweight
+    }
+    struct Child
+    {
+        public double Weight;
+        public Gender Gender;
+        public WeightClass WeightClass;
+    }
+    enum Gender
+    {
+     Male=1,
+     Female
     }
 }
