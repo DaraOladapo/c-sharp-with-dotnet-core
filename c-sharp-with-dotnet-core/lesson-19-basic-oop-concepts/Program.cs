96cbc140636cdd807934cf49b5d8490a0f2de9c6
Dara Oladapo <daraoladapo@outlook.com>
2019-07-11 08:24:35 +0100

    --generic + oopz

---
+++ c-sharp-with-dotnet-core/lesson-19-basic-oop-concepts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_19_basic_oop_concepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
