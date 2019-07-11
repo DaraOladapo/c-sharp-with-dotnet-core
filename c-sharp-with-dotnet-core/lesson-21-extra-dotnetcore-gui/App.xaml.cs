29d6c74645378a50d53eb7c3e33e9cb5fa38cc43
Dara Oladapo <daraoladapo@outlook.com>
2019-07-11 16:04:36 +0100

    +extra: lesson 21

---
+++ c-sharp-with-dotnet-core/lesson-21-extra-dotnetcore-gui/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lesson_21_extra_dotnetcore_gui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
