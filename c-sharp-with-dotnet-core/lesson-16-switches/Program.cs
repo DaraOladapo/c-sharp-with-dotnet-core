87ec555598e84b3d06bf653b03e348f2fb73230c
Dara Oladapo <daraoladapo@outlook.com>
2019-07-11 12:31:26 +0100

    switchy

--- c-sharp-with-dotnet-core/lesson-16-switches/Program.cs
+++ c-sharp-with-dotnet-core/lesson-16-switches/Program.cs
@@ -6,7 +6,47 @@ namespace lesson_16_switches
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string DayCode="";
+           var DayToDay=DateTime.Now.DayOfWeek;
+            switch (DayToDay)
+            {
+                case DayOfWeek.Sunday:
+                DayCode="Sabbath Day";
+                    break;
+                case DayOfWeek.Monday:
+                DayCode="Man Crush Monday";
+                    break;
+                case DayOfWeek.Tuesday:
+                DayCode="Turnup Tuesday";
+                    break;
+                case DayOfWeek.Wednesday:
+                DayCode="Woman Crush Wednesday";
+                    break;
+                case DayOfWeek.Thursday:
+                DayCode="Throwback Thursday";
+                    break;
+                case DayOfWeek.Friday:
+                DayCode="Flashback Friday";
+                    break;
+                case DayOfWeek.Saturday:
+                DayCode="Owambe";
+                    break;
+                default:
+                DayCode="Just another day";
+                    break;
+            }
+            System.Console.WriteLine(DayCode);
         }
     }
+        enum DaysOfWeek
+        {
+         Sunday=1,
+         Monday,
+         Tuesday,
+         Wednesday,
+         Thursday,
+         Friday,
+         Saturday
+        }
+    
 }
